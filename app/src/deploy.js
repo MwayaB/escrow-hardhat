fefc24401ab409e2bd0391545cac952be86b0f91
Mwayanjana Bolokonya <mwayab@gmail.com>
2024-01-09 17:14:15 +0200

    first commit

--- app/src/deploy.js
+++ app/src/deploy.js
@@ -7,5 +7,7 @@ export default async function deploy(signer, arbiter, beneficiary, value) {
     Escrow.bytecode,
     signer
   );
-  return factory.deploy(arbiter, beneficiary, { value });
+
+  const amount = ethers.utils.parseEther(value, "Ether");
+  return factory.deploy(arbiter, beneficiary, { value: amount });
 }
